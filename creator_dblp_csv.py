5e288cc260c6685add2bb8768e7936adcecba580
Gianluigi Iannantuoni <56970452+diegogianlu@users.noreply.github.com>
2021-05-06 09:38:43 +0200

    Creator dblp file csv

---
+++ creator_dblp_csv.py
@@ -0,0 +1,84 @@
+import pandas as pd
+import pickle
+import glob
+import sys
+from lxml import etree
+import requests
+from bs4 import BeautifulSoup
+
+
+path = 'dataset'
+
+if len(glob.glob('dblp_new.xml')) == 0:
+    
+    myfile = open(path + '/dblp.xml', 'r')
+    outfile = open('dblp_new.xml', 'w')
+    ok = False
+
+    for line in myfile.readlines():
+        if "<inproceedings" in line or ok:
+            ok = True
+            if "</inproceedings" in line and ok:
+                if "<inproceedings" in line:
+                    ok = True
+                    outfile.write(line)
+                else:
+                    outfile.write('</inproceedings>\n')
+                    ok = False
+            else:
+                try:
+                    if line[1] == "/":
+                        outfile.write(line[line.find('>') + 1:])
+                    else:
+                        outfile.write(line)
+                except:
+                    print(line)
+    myfile.close()
+    outfile.close()
+
+
+db = []
+
+for event, element in etree.iterparse("dblp_new.xml", 
+                                      tag="inproceedings", load_dtd=True, html=True):
+   title = ''
+   year = ''
+   ee = ''
+   url = ''
+   author = []
+
+   for child in element:
+       if child.tag == 'author' and child.text:
+           author.append(child.text)
+       if child.tag == 'title' and child.text:
+           title = child.text
+       if child.tag == 'year' and child.text:
+           year = child.text
+       if child.tag == 'ee' and child.text:
+           ee = child.text
+       if child.tag == 'url' and child.text:
+           url = child.text
+ 
+   db.append({
+       'author': ','.join(author),
+       'title': title,
+       'year': year,
+       'ee': ee,
+       'url': url
+   })
+
+   element.clear()
+
+
+df_dblp = pd.DataFrame(db)
+df_dblp['doi'] = df_dblp['ee'].str.replace(r'https://doi.org/', '', regex=True)
+df_dblp = df_dblp.drop(['ee'], axis=1)
+df_dblp.rename(columns={'doi' : 'Articolo'}, inplace=True)
+
+
+df_dblp.to_csv('dblp.csv')
+
+
+
+
+
